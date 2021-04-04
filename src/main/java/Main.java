f8090db3be71e23dc9a799bfd2fd635376609b95
IgnatLT79 <71728683+IgnatLT79@users.noreply.github.com>
2021-04-04 19:11:17 +0300

    added another sout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is test");
         System.out.println("Another string to merge!");
+        System.out.println("Yet another merge");
     }
 }
